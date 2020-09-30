2d2a8eb95c639af1d66eeda256c07a42ffdb7f7d
diivantcov <71901588+diivantcov@users.noreply.github.com>
2020-09-30 23:14:58 +0300

    Добавил управление персонажем при помощи кнопок

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,10 +1,14 @@
 export class Character {
-  constructor() {}
+  constructor() {
+    document.addEventListener("keydown", this.handleKeyboardEvents.bind(this));
+  }
   size = {
     width: 50,
     height: 50,
   };
-  stats = {};
+  stats = {
+    speed: 10,
+  };
   coords = {
     posX: 100,
     posY: 100,
@@ -17,5 +21,37 @@ export class Character {
   }
   getSize() {
     return this.size;
+  }
+  handleKeyboardEvents(event) {
+    const { key } = event;
+    const shouldMoveUp = (key) =>
+      key === "ArrowUp" ||
+      key.toLowerCase() === "w" ||
+      key.toLowerCase() === "ц";
+    const shouldMoveDown = (key) =>
+      key === "ArrowDown" ||
+      key.toLowerCase() === "s" ||
+      key.toLowerCase() === "ы";
+    const shouldMoveLeft = (key) =>
+      key === "ArrowLeft" ||
+      key.toLowerCase() === "s" ||
+      key.toLowerCase() === "ы";
+    const shouldMoveRight = (key) =>
+      key === "ArrowRight" ||
+      key.toLowerCase() === "d" ||
+      key.toLowerCase() === "в";
+    const moveUp = () => (this.coords.posY -= this.speed);
+    const moveDown = () => (this.coords.posY += this.speed);
+    const moveLeft = () => (this.coords.posX -= this.speed);
+    const moveRight = () => (this.coords.posX += this.speed);
+    shouldMoveUp
+      ? moveUp()
+      : shouldMoveDown
+      ? moveDown()
+      : shouldMoveLeft
+      ? moveLeft()
+      : shouldMoveRight
+      ? moveRight()
+      : null;
   }
 }
